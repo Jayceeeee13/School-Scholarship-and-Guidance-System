704f1c6b74a2ff909519307e16ebce1e7100d48c
John Crisostomo Salazar <salazarjohncrisostomo1@gmail.com>
2026-08-16 00:52:36 +0000

    Fix trusted proxies for HTTPS session handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
         'enrolled' => \App\Http\Middleware\EnsureUserIsEnrolled::class,
     ]);
+
+	$middleware->trustProxies(at: '*');
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //

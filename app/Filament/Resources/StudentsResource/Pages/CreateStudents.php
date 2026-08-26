077ad7c32798aff268a3f8f024fec0e6ceab9927
John Crisostomo Salazar <salazarjohncrisostomo1@gmail.com>
2026-08-26 22:24:26 +0800

    create record

--- app/Filament/Resources/StudentsResource/Pages/CreateStudents.php
+++ app/Filament/Resources/StudentsResource/Pages/CreateStudents.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateStudents extends CreateRecord
 {
             protected static string $resource = StudentsResource::class;
+
+            protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
